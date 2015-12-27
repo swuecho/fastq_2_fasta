f4a9906a32f2b73cdd6cd16c86c9c82089f63549
Hao Wu <hwu@intellisurvey.com>
2015-12-27 11:52:26 -0800

    cpp done

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	gcc -o bin/fastq_2_fasta_c -s fastq_2_fasta_get_line.c
 	go build fastq_2_fasta_go.go; mv fastq_2_fasta_go bin/
+	g++ -o bin/fastq_2_fasta_cpp -s fastq_2_fasta_cpp.cpp
--- fastq_2_fasta_cpp.cpp
+++ fastq_2_fasta_cpp.cpp
@@ -4,10 +4,17 @@
 
 int main(int argc, char * argv[])
 {
-       std::ifstream file(argv[1]);
-       std::string line;
-         while (std::getline(file, line)) {
-           std::cout << line << std::endl;
-       }
-       return 0;
+    std::ifstream file(argv[1]);
+    std::string line;
+    int line_num = 0;
+    while (std::getline(file, line)) {
+         if ( line_num % 4 == 0) {
+            std::cout << line.replace(0,1,">") << std::endl;
+         }
+         if ( line_num % 4 == 1 ) {
+            std::cout << line << std::endl;
+         }
+       line_num++;
+    }
+    return 0;
 }
