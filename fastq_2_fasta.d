8f3403c8e3f286d76f2feaad37b41f7fc27eea0d
Hao Wu <hwu@intellisurvey.com>
2016-01-02 20:44:37 -0800

    fast still have error

---
+++ fastq_2_fasta.d
@@ -0,0 +1,20 @@
+import std.stdio;
+import std.string;
+
+void main(string[] args)
+{
+    File  file = File(args[1], "r");
+    int line_num = 0;
+    while (!file.eof()) {
+        string line = chomp(file.readln());
+        if ( line_num % 4 == 0) {
+            writeln(">", line[1..$]);
+            line_num = 0;
+        }
+        if ( line_num == 1 ) {
+            writeln(line);
+        }
+        line_num++;
+    }
+
+}
