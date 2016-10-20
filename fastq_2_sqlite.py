b37022b1e46425efa3a7e3e6e88cced9c29c657f
Hao Wu <hwu@intellisurvey.com>
2016-10-19 20:12:10 -0700

    file to fastq

--- fastq_2_sqlite.py
+++ fastq_2_sqlite.py
@@ -1,10 +1,11 @@
 import sqlite3
 from os import path 
 
-data_dir = '/home/hwu/dev/stock_data/data'
-conn = sqlite3.connect(path.join(data_dir, 'stock.db'))
+data_dir = '/home/hwu/dev'
+conn = sqlite3.connect(path.join(data_dir, 'seq.db'))
 
-with open(filename, "r") as ins:
+c = conn.cursor()
+with open("/home/hwu/dev/fastq_2_fasta/data/SRR062634.filt.fastq", "r") as ins:
     line_num = 0
     seq_id = ""
     seq = ""
@@ -15,7 +16,10 @@ with open(filename, "r") as ins:
             line_num = 0
         if (line_num  == 1):
             seq = line.strip() 
-        if (line_num  == 2):
+        if (line_num  == 3):
             score = line.strip()
+            c.execute("INSERT INTO seq (id, seq, score) VALUES(?, ?, ?)" , (seq_id, seq, score))
         line_num = line_num + 1
+
+conn.commit()
 
