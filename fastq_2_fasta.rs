14f1a6583795c7c14f6e12a9644f7b5a4db6d5dd
Hao Wu <hwu@intellisurvey.com>
2016-10-22 19:49:27 -0700

    rustfmt

--- fastq_2_fasta.rs
+++ fastq_2_fasta.rs
@@ -1,20 +1,20 @@
 use std::io::BufReader;
-    use std::io::BufRead;
-    use std::fs::File;
+use std::io::BufRead;
+use std::fs::File;
 
-    fn main() {   
-        let f = File::open("data/SRR062634.filt.fastq").unwrap();
-        let file = BufReader::new(&f);
-        for (num, line) in file.lines().enumerate() {
-            let l = line.unwrap();
-            if  num % 4 == 0 {
-                //let chars: String = l.chars().skip(1).collect(); 
-                //println!(">{}", chars);
-                println!(">{}", &l[1..]);
+fn main() {
+    let f = File::open("data/SRR062634.filt.fastq").unwrap();
+    let file = BufReader::new(&f);
+    for (num, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+        if num % 4 == 0 {
+            // let chars: String = l.chars().skip(1).collect();
+            // println!(">{}", chars);
+            println!(">{}", &l[1..]);
 
-            }
-            if  num % 4 == 1  {
-                println!("{}", l); 
-             }
-        }           
+        }
+        if num % 4 == 1 {
+            println!("{}", l);
+        }
     }
+}
