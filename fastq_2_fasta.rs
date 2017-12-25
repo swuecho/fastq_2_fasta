2e4bc70d162b93d36cf697a84782d9f415a5c087
Hao Wu <echowuhao@gmail.com>
2017-12-24 19:35:45 -0800

    update

--- fastq_2_fasta.rs
+++ fastq_2_fasta.rs
@@ -1,20 +1,26 @@
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
+use std::io;
+use std::io::Write;
 
 fn main() {
     let f = File::open("data/SRR062634.filt.fastq").unwrap();
     let file = BufReader::new(&f);
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+
     for (num, line) in file.lines().enumerate() {
         let l = line.unwrap();
         if num % 4 == 0 {
             // let chars: String = l.chars().skip(1).collect();
             // println!(">{}", chars);
-            println!(">{}", &l[1..]);
-
+            handle.write(&l[1..].as_bytes()).unwrap();
+            handle.write(b"\n").unwrap();
         }
         if num % 4 == 1 {
-            println!("{}", l);
+            handle.write(l.as_bytes()).unwrap();
+            handle.write(b"\n").unwrap();
         }
     }
 }
